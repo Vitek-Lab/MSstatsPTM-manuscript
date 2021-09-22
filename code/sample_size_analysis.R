6e416c79a9b64cec2f4facad47e3f605e8b43173
Devon Kohler <devonjkohler@gmail.com>
2021-09-22 11:52:10 -0400

    Added sample size plots to supplementary

--- code/sample_size_analysis.R
+++ code/sample_size_analysis.R
@@ -2,7 +2,7 @@
 
 ## Code directly from MSstats designSampleSize function
 ## Range of desired FC
-delta <- log2(seq(.5, 1.5, 0.025))
+delta <- log2(seq(1.5, 2.5, 0.025))
 desiredFC <- 2 ^ delta
 
 # delta <- seq(.5, 1.5, 0.025)
@@ -21,42 +21,58 @@ z_beta <- qnorm(power)
 aa <- (delta / (z_alpha + z_beta)) ^2
 
 #Specify sigma for ptm and unmod pep
-sigma_error_mod <- .2
-sigma_error_unmod <- .1
+
 
 ## Original code containing sigma for subject.. can include or assume 0 for now
 ## numSample = round(2 * (median_sigma_error + median_sigma_subject) / aa, 0)
 
-numSample_nomod <- round(2 * (sigma_error_mod) / aa, 0)
-numSample_mod <- round(2 * (sqrt(sigma_error_mod^2 + sigma_error_unmod^2)) / aa, 0)
+cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
 
-p1 <- data.frame("type" = c(rep("With adjustment", length(numSample_mod)),
-                      rep("Without adjustment", length(numSample_nomod))),
-           "Log2FC" = c(desiredFC, desiredFC),
-           "num_samples" = c(numSample_mod, numSample_nomod)) %>%
-  ggplot() + geom_line(aes(x = Log2FC, y = num_samples, group = type, color = type))
+numSample_nomod_1 <- round(2 * (.1) / aa, 0)
+numSample_nomod_2 <- round(2 * (.2) / aa, 0)
+numSample_nomod_3 <- round(2 * (.4) / aa, 0)
 
-sigma_error_mod <- .2
-sigma_error_unmod <- .2
+p1 <- data.frame("SD" = c(rep(".1", length(numSample_nomod_1)),
+                      rep(".2", length(numSample_nomod_1)),
+                      rep(".4", length(numSample_nomod_1))),
+           "Log2FC" = c(desiredFC, desiredFC, desiredFC),
+           "num_samples" = c(numSample_nomod_1, numSample_nomod_2, numSample_nomod_3)) %>%
+  ggplot() + geom_line(aes(x = Log2FC, y = num_samples, group = SD, color = SD), size = 1.2) +
+  scale_color_discrete(cbPalette) +
+  labs(title = "Replicates Needed to Reach Power of .9", y = "# of replicates per condition") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),
+        legend.text=element_text(size=12),
+        axis.title.y = element_text(size = 14),
+        axis.title.x = element_text(size = 14),
+        title = element_text(size = 16),
+        strip.text = element_text(size = 12))
+png("supplementary/sim_new/simple_power_analysis.png", width = 750, height = 500)
+p1
+dev.off()
 
-numSample_nomod <- round(2 * (sigma_error_mod + .3) / aa, 0)
-numSample_mod <- round(2 * (sqrt(sigma_error_mod^2 + sigma_error_unmod^2) + .3) / aa, 0)
+numSample_nomod_1 <- round((2 * sqrt(.2^2 + .1^2)) /  aa, 0)
+numSample_nomod_2 <- round((2 * sqrt(.2^2 + .2^2)) / aa, 0)
+numSample_nomod_3 <- round((2 * sqrt(.2^2 + .4^2)) / aa, 0)
+numSample_nomod_4 <- round((2 * .2) / aa, 0)
 
-p2 <- data.frame("type" = c(rep("With adjustment", length(numSample_mod)),
-                      rep("Without adjustment", length(numSample_nomod))),
-           "Log2FC" = c(delta, delta),
-           "num_samples" = c(numSample_mod, numSample_nomod)) %>%
-  ggplot() + geom_line(aes(x = Log2FC, y = num_samples, group = type, color = type))
+p2 <- data.frame("SD" = c(rep(".2, .1", length(numSample_nomod_1)),
+                          rep(".2, .2", length(numSample_nomod_1)),
+                          rep(".2, .4", length(numSample_nomod_1)),
+                          rep("Baseline - .2", length(numSample_nomod_1))),
+                 "Log2FC" = c(desiredFC, desiredFC, desiredFC, desiredFC),
+                 "num_samples" = c(numSample_nomod_1, numSample_nomod_2, numSample_nomod_3,
+                                   numSample_nomod_4)) %>%
+  ggplot() + geom_line(aes(x = Log2FC, y = num_samples, group = SD, color = SD)) +
+  labs(title = "Replicates Needed to Reach Power of .9", y = "# of replicates per condition") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),
+        legend.text=element_text(size=12),
+        axis.title.y = element_text(size = 14),
+        axis.title.x = element_text(size = 14),
+        title = element_text(size = 16),
+        strip.text = element_text(size = 12))
 
-sigma_error_mod <- .2
-sigma_error_unmod <- .4
-
-numSample_nomod <- round(2 * (sigma_error_mod + .05) / aa, 0)
-numSample_mod <- round(2 * (sqrt(sigma_error_mod^2 + sigma_error_unmod^2) + .05) / aa, 0)
-
-p3 <- data.frame("type" = c(rep("With adjustment", length(numSample_mod)),
-                      rep("Without adjustment", length(numSample_nomod))),
-           "Log2FC" = c(delta, delta),
-           "num_samples" = c(numSample_mod, numSample_nomod)) %>%
-  ggplot() + geom_line(aes(x = Log2FC, y = num_samples, group = type, color = type))
-
+p2
